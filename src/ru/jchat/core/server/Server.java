af0a011e95d1f03da5d7db6fc484a1e7583956d9
Dimio <dimio@dimio.org>
2019-04-11 15:21:24 +0300

    added logging with log4j on server

--- src/ru/jchat/core/server/Server.java
+++ src/ru/jchat/core/server/Server.java
@@ -1,5 +1,8 @@
 package ru.jchat.core.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,6 +10,8 @@ import java.sql.SQLException;
 import java.util.Vector;
 
 public class Server {
+    private static final Logger log = LogManager.getLogger(Server.class);
+
     private Vector<ClientHandler> clients;
     private AuthService authService;
 
@@ -19,16 +24,16 @@ public class Server {
             clients = new Vector<>();
             authService = new AuthService();
             authService.connect();
-            System.out.println("Server started... Waiting clients");
+            log.info("Server started... Waiting clients");
             while (true){
                 Socket socket = serverSocket.accept();
-                System.out.println("Client connected " + socket.getInetAddress() + " " + socket.getPort() + " " + socket.getLocalPort());
+                log.info("Client connected {} {} {}", socket.getInetAddress(), socket.getPort(), socket.getLocalPort());
                 new ClientHandler(this, socket);
             }
         } catch (IOException e){
             e.printStackTrace();
         } catch (SQLException | ClassNotFoundException e){
-            System.out.println("Не удалось запустить сервис авторизации");
+            log.debug("Не удалось запустить сервис авторизации");
             e.printStackTrace();
         } finally {
             authService.disconnect();
